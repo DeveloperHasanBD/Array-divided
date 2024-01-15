c66e585ce054e6a768e9fe9945be89e19a1c35e3
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2024-01-15 22:44:07 +0600

    Update index.php

--- index.php
+++ index.php
@@ -11,4 +11,26 @@ $pks_gallery_one = get_field('pks_gallery_one');
     if ($count_item > 1) {
         $second_items      = array_slice($pks_gallery_one, $divider, $count_item);
     }
+
+
+
+
+        // another problem 
+
+
+
+
+
+	$gallery_img_url 	= [];
+	$itemsPerSubArr = 5;
+	$numberOfSubArr = ceil(count($gallery_img_url) / $itemsPerSubArr);
+	$gallery_images 	 = array_fill(0, $numberOfSubArr, []);
+	for ($i = 0; $i < count($gallery_img_url); $i++) {
+		$index = floor($i / $itemsPerSubArr);
+		$gallery_images[$index][] = $gallery_img_url[$i];
+	}
+
+
+
+        
 ?>
